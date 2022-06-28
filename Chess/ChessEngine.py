fe94a1d6689f867ce9fcd7b1dac970cd2bd581cb
Dashwii <dashwiibox@gmail.com>
2022-06-28 08:05:36 -0400

    Initial Commit

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,288 @@
+class ChessEngine:
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.white_to_move = True
+        self.move_log = []
+        self.piece_moves_func = {"P": self.get_pawn_moves, "R": self.get_rook_moves,
+                                 "K": self.get_king_moves, "N": self.get_knight_moves,
+                                 "Q": self.get_queen_moves, "B": self.get_bishop_moves}
+        self.current_valid_moves = self.get_valid_moves()
+
+    """
+    Takes a Move as a parameter and executes it (will not work for castling, pawn promotion, and en-passant"""
+    def do_move(self, move):
+        self.board[move.start_row][move.start_col] = "--"
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.move_log.append(move)
+        self.white_to_move = not self.white_to_move
+        self.refresh_valid_moves()
+
+    """
+    Undo the last move made"""
+    def undo_move(self):
+        if len(self.move_log):
+            last_move = self.move_log.pop()
+            self.board[last_move.start_row][last_move.start_col] = last_move.piece_moved
+            self.board[last_move.end_row][last_move.end_col] = last_move.piece_captured
+            self.white_to_move = not self.white_to_move
+            self.refresh_valid_moves()
+
+    """
+    Will determine all valid moves considering king check"""
+    def get_valid_moves(self):
+        return self.get_all_possible_moves()  # Temporary. Will check and remove moves that put the king in check later.
+
+    """
+    Will determine all possible moves not considering king check"""
+    def get_all_possible_moves(self):
+        moves = []
+        for i, r in enumerate(self.board):  # Num of rows
+            for j, c in enumerate(r):  # Num of columns
+                turn = self.board[i][j][0]
+                if (turn == "w" and self.white_to_move) or (turn == "b" and not self.white_to_move):
+                    piece = self.board[i][j][1]
+                    moves.extend(self.piece_moves_func[piece](i, j))
+        return moves
+
+    def refresh_valid_moves(self):
+        self.current_valid_moves = self.get_valid_moves()
+
+    """
+    Returns all moves for the pawn piece in the row and column. Will search for diagonal captures and vertical movement."""
+    def get_pawn_moves(self, r, c):
+        moves = []
+        turn = self.board[r][c][0]
+        # Search for every column above the pawn. If nothing is blocking it it's a valid move.
+        for i, _ in enumerate(self.board):
+            if turn == "w":  # Go up the column from whites perspective
+                try:
+                    if r == 6 and self.board[r - 2][c] == "--":
+                        moves.append(Move((r, c), (r - 2, c), self.board))
+                    if self.board[r - 1][c] == "--":
+                        moves.append(Move((r, c), (r - 1, c), self.board))
+                    if self.board[r - 1][c - 1] != "--" and self.board[r - 1][c - 1][0] == "b":
+                        moves.append(Move((r, c),(r - 1, c - 1), self.board))
+                    if self.board[r - 1][c + 1] != "--" and self.board[r - 1][c + 1][0] == "b":
+                        moves.append(Move((r, c), (r - 1, c + 1), self.board))
+                except IndexError:
+                    pass
+            else:
+                try:
+                    if r == 1 and self.board[r + 2][c] == "--":
+                        moves.append(Move((r, c), (r + 2, c), self.board))
+                    if self.board[r + 1][c] == "--":  # Go down the column from whites perspective
+                        moves.append(Move((r, c), (r + 1, c), self.board))
+                    if self.board[r + 1][c - 1] != "--" and self.board[r + 1][c - 1][0] == "w":
+                        moves.append(Move((r, c),(r + 1, c - 1), self.board))
+                    if self.board[r + 1][c + 1] != "--" and self.board[r + 1][c + 1][0] == "w":
+                        moves.append(Move((r, c), (r + 1, c + 1), self.board))
+                except IndexError:
+                    pass
+        return moves
+
+    """
+    Returns all possible moves for the rook. Will search vertically and horizontally."""
+    def get_rook_moves(self, r, c):
+        moves = []
+        moves.extend(self.horizontal_scan(r, c))
+        moves.extend(self.horizontal_scan(r, c, reverse=True))
+        return moves
+
+    def get_king_moves(self, r, c):
+        moves = []
+        vectors = [(-1, -1), (0, -1), (-1, 0), (1, 0), (0, 1), (-1, 1), (1, -1), (1, 1)]
+        for direction in vectors:
+            dx = c + direction[1]
+            dy = r + direction[0]
+            if 0 <= dx < len(self.board) and 0 <= dy < len(self.board):
+                sq_occupy = self.check_if_square_has_enemy_piece((r, c), (dy, dx))
+                if sq_occupy == "EMPTY" or sq_occupy == "ENEMY":
+                    moves.append(Move((r, c), (dy, dx), self.board))
+                else:
+                    pass
+        return moves
+
+    def get_queen_moves(self, r, c):
+        moves = []
+        moves.extend(self.diagonal_scan(r, c))
+        moves.extend(self.horizontal_scan(r, c))
+        moves.extend(self.horizontal_scan(r, c, reverse=True))
+        return moves
+
+    def get_knight_moves(self, r, c):
+        moves = []
+
+        def column_knight_check(current_r, comparing_r, r_abs_dist, c, board):
+            _moves = []
+            if c + 1 < len(board):
+                if c + 2 < len(board):
+                    if self.check_if_square_has_enemy_piece((current_r, c), (comparing_r, c + 2)) != "FRIENDLY" and r_abs_dist == 1:
+                        _moves.append(Move((current_r, c), (comparing_r, c + 2), board))
+                if self.check_if_square_has_enemy_piece((current_r, c), (comparing_r, c + 1)) != "FRIENDLY" and r_abs_dist == 2:
+                    _moves.append(Move((current_r, c), (comparing_r, c + 1), board))
+
+            if c - 1 >= 0:
+                if c - 2 >= 0:
+                    if self.check_if_square_has_enemy_piece((current_r, c), (comparing_r, c - 2)) != "FRIENDLY" and r_abs_dist == 1:
+                        _moves.append(Move((current_r, c), (comparing_r, c - 2), board))
+                if self.check_if_square_has_enemy_piece((current_r, c), (comparing_r, c - 1)) != "FRIENDLY" and r_abs_dist == 2:
+                    _moves.append(Move((current_r, c), (comparing_r, c - 1), board))
+            return _moves
+
+        if r - 1 >= 0:
+            if r - 2 >= 0:
+                moves.extend(column_knight_check(r, r - 2, 2, c, self.board))
+            moves.extend(column_knight_check(r, r - 1, 1, c, self.board))
+        if r + 1 < len(self.board):
+            if r + 2 < len(self.board):
+                moves.extend(column_knight_check(r, r + 2, 2, c, self.board))
+            moves.extend(column_knight_check(r, r + 1, 1, c, self.board))
+        return moves
+
+    def get_bishop_moves(self, r, c):
+        return self.diagonal_scan(r, c)
+
+    def diagonal_scan(self, r, c):
+        moves = []
+        vectors = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        for direction in vectors:
+            for i, row in enumerate(self.board[r]):
+                i += 1
+                dx = c + (i * direction[1])  # Column in list
+                dy = r + (i * direction[0])  # Row in list
+                if 0 <= dx < len(self.board) and 0 <= dy < len(self.board):
+                    if self.board[dy][dx] == "--":
+                        moves.append(Move((r, c), (dy, dx), self.board))
+                    else:
+                        our_turn = self.board[r][c][0]
+                        other_turn = self.board[dy][dx][0]
+                        if our_turn == other_turn:
+                            break
+                        else:
+                            moves.append(Move((r, c), (dy, dx), self.board))
+                            break
+                else:
+                    break
+        return moves
+
+    def horizontal_scan(self, r, c, reverse=False):
+        _moves = []
+
+        """
+        Row check"""
+        if not reverse:
+            for i in range(len(self.board[r][c:])):  # Scanning right
+                i += 1
+                if 0 <= c + i < len(self.board):
+                    if self.board[r][c + i] == "--":
+                        _moves.append(Move((r, c), (r, c + i), self.board))
+                    elif self.board[r][c + i] != "--":
+                        our_turn = self.board[r][c][0]
+                        other_turn = self.board[r][c + i][0]
+                        if our_turn == other_turn:
+                            break
+                        else:
+                            _moves.append(Move((r, c), (r, c + i), self.board))
+                            break
+                else:
+                    break
+        else:
+            for i in range(len(self.board[r][:c + 1])):  # Scanning left
+                i += 1
+                if 0 <= c - i < len(self.board):
+                    if self.board[r][c - i] == "--":
+                        _moves.append(Move((r, c), (r, c - i), self.board))
+                    elif self.board[r][c - i] != "--":
+                        our_turn = self.board[r][c][0]
+                        other_turn = self.board[r][c - i][0]
+                        if our_turn == other_turn:
+                            break
+                        else:
+                            _moves.append(Move((r, c), (r, c - i), self.board))
+                            break
+                else:
+                    break
+
+        """
+        Column check"""
+        if not reverse:
+            for i in range(len(self.board[r:])):  # Scan down the column
+                i += 1
+                if 0 <= r + i < len(self.board):
+                    if self.board[r + i][c] == "--":
+                        _moves.append(Move((r, c), (r + i, c), self.board))
+                    elif self.board[r + i][c] != "--":
+                        our_turn = self.board[r][c][0]
+                        other_turn = self.board[r + i][c][0]
+                        if our_turn == other_turn:
+                            break
+                        else:
+                            _moves.append(Move((r, c), (r + i, c), self.board))
+                            break
+                else:
+                    break
+        else:
+            for i in range(len(self.board[:r])):  # Scan up the column
+                i += 1
+                if 0 <= r - i < len(self.board):
+                    if self.board[r - i][c] == "--":
+                        _moves.append(Move((r, c), (r - i, c), self.board))
+                    elif self.board[r - i][c] != "--":
+                        our_turn = self.board[r][c][0]
+                        other_turn = self.board[r - i][c][0]
+                        if our_turn == other_turn:
+                            break
+                        else:
+                            _moves.append(Move((r, c), (r - i, c), self.board))
+                            break
+
+        return _moves
+
+    def check_if_square_has_enemy_piece(self, start_sq, end_sq):
+        # Function will soon acknowledge kings.
+        start_r = start_sq[0]
+        start_c = start_sq[1]
+        end_r = end_sq[0]
+        end_c = end_sq[1]
+
+        our_turn = self.board[start_r][start_c][0]
+        other_turn = self.board[end_r][end_c][0]
+
+        if other_turn == "-":
+            return "EMPTY"
+        elif other_turn == our_turn:
+            return "FRIENDLY"
+        else:
+            return "ENEMY"
+
+
+class Move:
+    # Computer notation to chess notation conversion
+    rank_to_row = {"8": 0, "7": 1, "6": 2, "5": 3, "4": 4,
+                   "3": 5, "2": 6, "1": 7}
+    row_to_rank = {v: k for k, v in rank_to_row.items()}
+    file_to_col = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    col_to_file = {v: k for k, v in file_to_col.items()}
+
+    def __init__(self, start_sq, end_sq, chess_board):
+        self.start_row = start_sq[0]
+        self.start_col = start_sq[1]
+        self.end_row = end_sq[0]
+        self.end_col = end_sq[1]
+
+        self.piece_moved = chess_board[self.start_row][self.start_col]
+        self.piece_captured = chess_board[self.end_row][self.end_col]
+        self.id = str(self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col)
+
+    def get_chess_notation(self):
+        return Move.col_to_file[self.start_col] + Move.row_to_rank[self.start_row] + Move.col_to_file[self.end_col] + Move.row_to_rank[self.end_row]
---
+++ Chess/ChessMain.py
@@ -0,0 +1,127 @@
+import pygame as p
+from ChessEngine import ChessEngine, Move
+
+p.init()
+p.mixer.init()
+move_sound = p.mixer.Sound("move_sound.mp3")
+WIDTH, HEIGHT = 1920, 1080
+BOARD_WIDTH = BOARD_HEIGHT = 1024
+SQ_SIZE = 128
+FPS = 60
+CLOCK = p.time.Clock()
+IMAGES = {}
+
+BOARD_X = WIDTH // 2 - BOARD_WIDTH // 2
+BOARD_Y = HEIGHT // 2 - BOARD_HEIGHT // 2
+
+font = p.font.SysFont("Comic Sans MS", 50)
+current_turn_text = font.render("Current Turn:", True, "white")
+white_text = font.render("White", True, "white")
+black_text = font.render("Black", True, "white")
+
+
+def load_images():
+    # Load images once into a dictionary for later access.
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bP", "wR", "wN", "wB", "wQ", "wK", "wP"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(f"images/{piece}.png"), (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+    load_images()
+    running = True
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    start_sq = ()
+    gs = ChessEngine()
+    while running:
+        CLOCK.tick(FPS)
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            if e.type == p.MOUSEBUTTONDOWN:
+                start_sq = (click_sq_coordinates())
+            elif e.type == p.MOUSEBUTTONUP and e.button == 1:
+                end_sq = (click_sq_coordinates())
+                if end_sq == start_sq or end_sq is None or gs.board[start_sq[0]][start_sq[1]] == "--":
+                    start_sq = ()
+                else:
+                    move = Move(start_sq, end_sq, gs.board)
+                    for i in gs.current_valid_moves:
+                        if move.id == i.id:
+                            move_sound.play()
+                            gs.do_move(move)
+                            start_sq = ()
+                            break
+                        else:
+                            start_sq = ()
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undo_move()
+
+        screen.fill((64, 64, 64))
+        draw_board(screen, gs.board, start_sq)
+        draw_pieces(screen, gs.board, gs, start_sq)
+        screen.blit(current_turn_text, p.Rect(0, 40, 30, 30))
+        if gs.white_to_move:
+            screen.blit(white_text, p.Rect(0, 120, 30, 30))
+        else:
+            screen.blit(black_text, p.Rect(0, 120, 30, 30))
+        p.display.flip()
+
+
+def click_sq_coordinates():
+    click_pos = p.mouse.get_pos()
+    if BOARD_X < click_pos[0] < BOARD_X + BOARD_WIDTH:
+        if BOARD_Y < click_pos[1] < BOARD_Y + BOARD_HEIGHT:  # Check if area player clicked is in the chess board
+            row_clicked = (click_pos[1] - BOARD_Y) // SQ_SIZE
+            col_clicked = (click_pos[0] - BOARD_X) // SQ_SIZE
+            return row_clicked, col_clicked
+
+
+def draw_board(screen, chess_board, start_sq):
+    for i, r in enumerate(chess_board):
+        for j, c in enumerate(r):
+            if (i + j) % 2 == 0:
+                p.draw.rect(screen, "white", p.Rect(BOARD_X + (j * SQ_SIZE), (BOARD_Y + (i * SQ_SIZE)), SQ_SIZE, SQ_SIZE))
+            else:
+                p.draw.rect(screen, "dark green", p.Rect(BOARD_X + (j * SQ_SIZE), (BOARD_Y + (i * SQ_SIZE)), SQ_SIZE, SQ_SIZE))
+            if start_sq and i == start_sq[0] and j == start_sq[1] and chess_board[start_sq[0]][start_sq[1]] != "--":
+                p.draw.rect(screen, "red",
+                            p.Rect(BOARD_X + (j * SQ_SIZE), (BOARD_Y + (i * SQ_SIZE)), SQ_SIZE, SQ_SIZE))
+
+
+def draw_pieces(screen, chess_board, gs, start_sq):
+    # Loop through every square in the board and see what square gives us a valid spot. Pretty bad algo but will work for now
+    for i, r in enumerate(chess_board):
+        for j, c in enumerate(r):
+            if len(start_sq) and Move(start_sq, (i, j), chess_board).id in [d.id for d in gs.current_valid_moves]:
+                if chess_board[i][j] == "--":
+                    p.draw.circle(screen, "dark gray",
+                                  (SQ_SIZE // 2 + BOARD_X + (j * SQ_SIZE), SQ_SIZE // 2 + BOARD_Y + (i * SQ_SIZE)), 20)
+                else:
+                    p.draw.circle(screen, "dark gray",
+                                  (SQ_SIZE // 2 + BOARD_X + (j * SQ_SIZE), SQ_SIZE // 2 + BOARD_Y + (i * SQ_SIZE)), 60, width=12)
+
+    # Render pieces on the board
+    for i, r in enumerate(chess_board):
+        for j, c in enumerate(r):
+            piece = chess_board[i][j]
+            if start_sq is not None and len(start_sq) == 2 and i == start_sq[0] and j == start_sq[1]:
+                pass
+            else:
+                if piece != "--":
+                    screen.blit(IMAGES[piece], p.Rect(BOARD_X + (j * SQ_SIZE), BOARD_Y + (i * SQ_SIZE), SQ_SIZE, SQ_SIZE))
+
+    # Drag piece render
+    if start_sq:
+        piece = chess_board[start_sq[0]][start_sq[1]]  # Reverse column and row to get the actual piece we clicked on.
+        if piece != "--":
+            mouse_pos = p.mouse.get_pos()
+            square_highlight_pos = click_sq_coordinates()
+            if square_highlight_pos is not None:
+                p.draw.rect(screen, "blue", p.Rect(BOARD_X + square_highlight_pos[1] * SQ_SIZE, BOARD_Y + square_highlight_pos[0] * SQ_SIZE, SQ_SIZE, SQ_SIZE), 6) # Invert row and columns to get actual screen representation of highlight position.
+            screen.blit(IMAGES[piece], p.Rect(mouse_pos[0] - SQ_SIZE // 2, mouse_pos[1] - SQ_SIZE // 2, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == "__main__":
+    main()
